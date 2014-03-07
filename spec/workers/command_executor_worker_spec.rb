359e7e5bc95a53847312ecaf7e7928ac5893082d
Jakub Oboza <jakub.oboza@gmail.com>
2014-03-07 21:09:48 +0000

    added spec testing what happens when build explodes in worker

--- spec/workers/command_executor_worker_spec.rb
+++ spec/workers/command_executor_worker_spec.rb
@@ -10,4 +10,12 @@ describe CommandExecutor do
     build.output.should_not be_nil
   end
 
+  it "handles it ok when build explodes" do
+    build = Build.create(:command => "ls -alrt", :remote => "localhost")
+    Rush::Box.should_receive(:new).and_raise("unable to perform!")
+    CommandExecutor.perform(build.id)
+    build.reload
+    build.state.should == "failed"
+  end
+
 end
